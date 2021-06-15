78c7b7e8f44e7293b7332bfdeb257811221ae196
m666362 <mdrayhan9464@gmail.com>
2021-06-14 23:51:03 -0700

    verification code sent

---
+++ app/src/main/java/com/rich_it/library/Activity/MainActivity.java
@@ -0,0 +1,83 @@
+package com.rich_it.library.Activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.rich_it.library.R;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button sendOtp;
+    EditText phoneNoEditText;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sendOtp = findViewById(R.id.sendOtp);
+        mAuth = FirebaseAuth.getInstance();
+        phoneNoEditText = findViewById(R.id.phoneNoET);
+        sendOtp.setOnClickListener(this::onClick);
+    }
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallback =
+            new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+                @Override
+                public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                    super.onCodeSent(s, forceResendingToken);
+                    Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+
+                    String code = phoneAuthCredential.getSmsCode();
+                    Toast.makeText(MainActivity.this, code, Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onVerificationFailed(FirebaseException e) {
+                    Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                }
+            };
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.sendOtp:
+                Toast.makeText(this, phoneNoEditText.getText().toString().trim(), Toast.LENGTH_SHORT).show();
+                String phoneNumber = "+88" + phoneNoEditText.getText().toString().trim();
+                sendVerificationCodeToUser(phoneNumber);
+                break;
+        }
+    }
+
+    private void sendVerificationCodeToUser(String phoneNumber) {
+        PhoneAuthOptions options = PhoneAuthOptions.newBuilder(mAuth)
+                .setPhoneNumber(phoneNumber)       // Phone number to verify
+                .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                .setActivity(MainActivity.this)                 // Activity (for callback binding)
+                .setCallbacks(mCallback)          // OnVerificationStateChangedCallbacks
+                .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
+
+}
