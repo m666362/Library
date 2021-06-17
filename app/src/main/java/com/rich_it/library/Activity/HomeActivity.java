b0b39e4f56d81a732416d066991dfb150b2426f3
m666362 <mdrayhan9464@gmail.com>
2021-06-16 19:18:03 -0700

    got preset location

--- app/src/main/java/com/rich_it/library/Activity/HomeActivity.java
+++ app/src/main/java/com/rich_it/library/Activity/HomeActivity.java
@@ -2,15 +2,39 @@ package com.rich_it.library.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.rich_it.library.R;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button getLocationButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        initObject();
+        requiredTask();
+    }
+
+    private void initObject() {
+        getLocationButton = findViewById(R.id.getLocationButton);
+    }
+
+    private void requiredTask() {
+        getLocationButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.getLocationButton:
+                Intent intent = new Intent(this, MapsActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
