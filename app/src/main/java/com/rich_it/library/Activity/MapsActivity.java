ac2a41a865bbd86bbd3341a39dcf1d40af3659c8
m666362 <mdrayhan9464@gmail.com>
2021-06-20 19:36:14 +0600

    my location

--- app/src/main/java/com/rich_it/library/Activity/MapsActivity.java
+++ app/src/main/java/com/rich_it/library/Activity/MapsActivity.java
@@ -1,34 +1,116 @@
 package com.rich_it.library.Activity;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 import com.rich_it.library.R;
 import com.rich_it.library.databinding.ActivityMapsBinding;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Array;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    String TAG = MapsActivity.class.getName();
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    SupportMapFragment mapFragment;
+    FusedLocationProviderClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: ");
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        Log.d(TAG, "onPermissionGranted: ");
+                        getMyLocation();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                        Log.d(TAG, "onPermissionDenied: ");
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                        Log.d(TAG, "onPermissionRationaleShouldBeShown: ");
+                    }
+                }).check();
+    }
+
+    private void getMyLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+
+
+            Log.d(TAG, "getMyLocation: ");
+
+//            int[] grantResults = new Array();
+//            onRequestPermissionsResult(1, Manifest.permission.ACCESS_FINE_LOCATION, [1,2,3]);
+            return;
+        }
+        Task<Location> tasks = client.getLastLocation();
+        tasks.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                Log.d(TAG, "onSuccess: ");
+                mapFragment.getMapAsync(new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(@NonNull @NotNull GoogleMap googleMap) {
+                        Log.d(TAG, "onMapReady: ");
+                        mMap = googleMap;
+
+                        // Add a marker in Sydney and move the camera
+                        LatLng sydney = new LatLng(location.getLatitude(), location.getLongitude());
+                        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15));
+                    }
+                });
+            }
+        });
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
     }
 
     /**
@@ -42,6 +124,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        Log.d(TAG, "onMapReady: ");
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
