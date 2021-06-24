879e2f3b1b50653b0c22ef663e53efca25e11c74
m666362 <mdrayhan9464@gmail.com>
2021-06-25 05:24:00 +0600

    kotlin environment & splash Runnable

--- app/src/main/java/com/rich_it/library/Activity/SplashActivity.java
+++ app/src/main/java/com/rich_it/library/Activity/SplashActivity.java
@@ -2,15 +2,28 @@ package com.rich_it.library.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
 
 import com.rich_it.library.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static int splashTimeOut = 4000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Toast.makeText(this, "Splash Activity", Toast.LENGTH_LONG).show();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, splashTimeOut);
     }
 }
